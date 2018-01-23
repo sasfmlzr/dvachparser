fbc18df1c8b6f9958f6daf13e850bb8bf71192b5
sasfmlzr <sasfmlzr@yandex.ru>
2018-01-23 17:31:25 +0300

    add model types of file for selective download

--- src/main/java/com/fomenko/webmplayer/api/model/DvachModel.java
+++ src/main/java/com/fomenko/webmplayer/api/model/DvachModel.java
@@ -8,17 +8,46 @@ public final class DvachModel extends Dvach {
         b,fet,hc,e,a,fur, gg
     }
 
-    public enum filterFile{
-        jpg,webm,all
+    public enum filterFile {
+        image("jpg","png"),
+        video("webm","mp4") ,
+        all("jpg","png","webm","mp4") ;
+        private final String[] typeFiles;
+        filterFile(String... typeFiles) {
+            this.typeFiles = typeFiles;
+        }
+        public String[] getTypeFiles() {
+            return typeFiles;
+        }
     }
+
+
 
     public String getBoard(String board){
         try {
             return boardEnum.valueOf(board).toString();
         }catch (IllegalArgumentException e) {
-            System.out.println("Беда");
+            System.out.println("Такой борды нет");
         }
        return null;
+    }
+   /* @Deprecated
+    public String getFilterFile(String typeFile){
+        try {
+            return filterFile.valueOf(typeFile).toString();
+        }catch (IllegalArgumentException e) {
+            System.out.println("Такого типа файлов нет");
+        }
+        return null;
+    }
+*/
+    public String[] getFilterFile(String typeFile){
+        try {
+            return filterFile.valueOf(typeFile).getTypeFiles();
+        }catch (IllegalArgumentException e) {
+            System.out.println("Такого типа файлов нет");
+        }
+        return null;
     }
 
     public String getThreads(String board){
