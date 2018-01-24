9037aaf36e6e41ca2313c24f79bda631d0c6d79f
sasfmlzr <sasfmlzr@yandex.ru>
2018-01-24 20:52:34 +0300

    added cookies for extended parsing boards

--- src/main/java/com/fomenko/webmplayer/api/controller/HttpOperation.java
+++ src/main/java/com/fomenko/webmplayer/api/controller/HttpOperation.java
@@ -7,15 +7,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+@SuppressWarnings("Duplicates")
 public final class HttpOperation extends Dvach {
 
     public String getRequest(String url) {
         URL obj;
         try {
             obj = new URL(url);
+
             HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
             connection.setRequestMethod("GET");
+            if(connection.getResponseCode()!=404){
+                InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+                BufferedReader in = new BufferedReader(inputStreamReader);
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            }
+            return "404";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Итератор сбился");
+        }
+        return "Ошибка запроса";
+    }
+    public String getRequest(String url, String cookie) {
+        URL obj;
+        try {
+            obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Cookie",cookie);
             if(connection.getResponseCode()!=404){
                 InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
                 BufferedReader in = new BufferedReader(inputStreamReader);
