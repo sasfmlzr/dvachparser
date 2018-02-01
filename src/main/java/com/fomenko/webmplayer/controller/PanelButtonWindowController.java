40af83e738a0906f754a74640fab96d9ec796a41
sasfmlzr <sasfmlzr@yandex.ru>
2018-02-02 00:36:16 +0300

    Prepare to JavaFX interface

---
+++ src/main/java/com/fomenko/webmplayer/controller/PanelButtonWindowController.java
@@ -0,0 +1,15 @@
+package com.fomenko.webmplayer.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PanelButtonWindowController extends AnchorPane implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/com/fomenko/webmplayer/controller/VideoWindowController.java
@@ -0,0 +1,16 @@
+package com.fomenko.webmplayer.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class VideoWindowController extends AnchorPane implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
