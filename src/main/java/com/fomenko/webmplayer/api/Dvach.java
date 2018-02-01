353147ced1d94a87f01344cc68b000c0bb988ae8
sasfmlzr <sasfmlzr@yandex.ru>
2018-02-02 00:32:15 +0300

    add automate method

--- src/main/java/com/fomenko/webmplayer/api/Dvach.java
+++ src/main/java/com/fomenko/webmplayer/api/Dvach.java
@@ -5,6 +5,9 @@ import com.fomenko.webmplayer.api.controller.HttpOperation;
 import com.fomenko.webmplayer.api.controller.ParseDvach;
 import com.fomenko.webmplayer.api.model.DvachModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Dvach {
     public Dvach(){
         dvachModel= new DvachModel(this);
@@ -21,4 +24,24 @@ public class Dvach {
 /*  public DvachModel getDvachModel() {
         return dvachModel;
     }*/
+    public void parseDvachAutomate(String board, String typeFileSave, String cookie){
+        //String cookie = "usercode_auth=c4879607e360cf191118c33a66832583";
+        this.getDvachModel().setCookie(cookie);
+
+        this.getDvachModel().getBoard(board);     //Создался элемент двача
+
+        String threads = this.getDvachModel().getThreads(board);   //Получили борду
+
+        String response = this.httpOperation().getRequest(threads, cookie);     //Получили список тредов
+
+        ArrayList<String> threadsList = this.parseDvach().getNumThread(response);      //Получили номера тредов
+
+        HashMap<String,String> listLinkVideo = this.parseDvach().getListLinkVideoMd5(threadsList,board);    //Получили список файлов в тредах
+
+        HashMap<String,String> result = this.parseDvach().findDownloadedVideo(listLinkVideo, board);    //Получили исключающие треды
+
+        this.fileOperation().fileSave(new ArrayList<>(result.keySet()), typeFileSave); // video, image, all
+
+        System.out.println("Загрузили в файл результат");
+    }
 }
